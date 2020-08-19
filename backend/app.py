be1f3c0a01787eb0a5055c357809a99698b8de8c
DoubleHelix <tjrnyc5@gmail.com>
2020-08-19 19:03:27 -0400

    configuring

--- backend/app.py
+++ backend/app.py
@@ -77,10 +77,16 @@ def create_app(test_config=None):
     
 
     #* ----------------------- Access-Control-Allow configuration------------------------------#
+    # @app.after_request
+    # def after_request(response):
+    #     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,true')
+    #     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS,PATCH')
+    #     return response
+    
     @app.after_request
     def after_request(response):
-        response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,true')
-        response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS,PATCH')
+        response.headers.add('Access-Control-Allow-Headers','Content-Type,Authorization,true')
+        response.headers.add('Access-Control-Allow-Methods','GET,POST,PATCH,PUT,DELETE,OPTIONS')
         return response
     # * -------------------------------------- END -------------------------------------------- *
 
