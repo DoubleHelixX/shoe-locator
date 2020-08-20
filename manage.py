663415e3e40054692b8ca13ac3e34e1d7f584b32
DoubleHelix <tjrnyc5@gmail.com>
2020-08-19 21:48:10 -0400

    eh

--- manage.py
+++ manage.py
@@ -19,5 +19,5 @@ def migration(app,db):
 # manager = Manager(app)
 # manager.add_command('db', MigrateCommand) 
    
-if __name__ == '__main__':
-    manager.run()
+# if __name__ == '__main__':
+#     manager.run()
