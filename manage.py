6d17a94b664d7dbcfbab592d58808ba2465a9a1b
DoubleHelix <tjrnyc5@gmail.com>
2020-08-19 22:25:00 -0400

    changes

--- manage.py
+++ manage.py
@@ -10,4 +10,31 @@ manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
     manager.run()
-    
+
+
+
+
+
+# #----------------------------------------------------------------------------#
+# #* Migration Imports
+# #----------------------------------------------------------------------------#
+
+# from flask_script import Manager
+# from flask_migrate import Migrate, MigrateCommand
+# # from app import app
+# # from models import db
+
+# #----------------------------------------------------------------------------#
+# #* Manage Migration
+# #----------------------------------------------------------------------------#
+# def migration(app,db):
+#     migrate = Migrate(app, db)
+#     manager = Manager(app)
+#     manager.add_command('db', MigrateCommand)
+
+# # migrate = Migrate(app, db)
+# # manager = Manager(app)
+# # manager.add_command('db', MigrateCommand) 
+   
+# # if __name__ == '__main__':
+# #     manager.run()
